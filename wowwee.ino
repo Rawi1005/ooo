edcac6c09319a0d06cc5beec0f3a39d5b3f7a01c
Rawi1005 <101038560+Rawi1005@users.noreply.github.com>
2024-02-01 13:43:54 +0700

    Add files via upload
    1.1

--- wowwee.ino
+++ wowwee.ino
@@ -1,11 +1,11 @@
 int gas = 39;
-
-
+int led = 2;
+int limit = 2000;
 
 void setup() {
  Serial.begin(9600);
  pinMode(gas,INPUT); // INPUT = "justfriend";
-
+ pinMode(led, OUTPUT); 
 }
 
 void loop() {
@@ -13,4 +13,13 @@ void loop() {
    Serial.println(db);
    delay(500);
 
+   if (db >= limit){
+    Serial.println("OMG FIRE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    analogWrite(led, HIGH);
+   }
+
+   else{
+     analogWrite(led, LOW);
+   }
+   
 }
